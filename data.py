fda31c3a019b24e59fda1514db4f22cddd2b655b
Santiago Benoit <santib27@gmail.com>
2017-10-04 19:17:09 -0400

    more work on model and data generation

--- data.py
+++ data.py
@@ -1,11 +1,61 @@
+import click
+import glob
+import mido
 import numpy
+import os
 
 notes = 24
 
-def load_chords(midi_dir):
-    test = numpy.zeros((10, 4, notes)) # for testing purposes
-    return test
-    
 
-if __name__ == '__main__':
-    pass
+def chords_from_midi(midi_file):
+    chords = []
+    notes_on = []
+    midi = mido.MidiFile(midi_file)
+    for track in midi.tracks:
+        if track.name == 'Chords':
+            previous_type = None
+            for message in track:
+                if message.type == 'note_on':
+                    note = message.note
+                    if note not in notes_on:
+                        notes_on.append(note)
+                if message.type == 'note_off':
+                    if previous_type == 'note_on':
+                        chords.append(notes_on)
+                    note = message.note
+                    if note in notes_on:
+                        notes_on = notes_on[:]
+                        notes_on.remove(note)
+                if message.type == 'end_of_track':
+                    chords.append(notes_on)
+                previous_type = message.type
+    return chords
+
+
+def encode_chords(chords):
+    encoded = numpy.zeros((len(chords), notes))
+    minimum = min(min(x) for x in chords)
+    for i, chord in enumerate(chords):
+        for note in chord:
+            encoded[i, note - minimum] = 1
+    return encoded
+
+
+def load_chords(midi_dir):
+    progressions = []
+    midi_files = sorted(glob.glob(os.path.join(midi_dir, '*.mid')) + glob.glob(os.path.join(midi_dir, '*.midi')))
+    for midi_file in midi_files:
+        try:
+            chords = chords_from_midi(midi_file)
+            encoded = encode_chords(chords)
+            progressions.append(encoded)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("Skipping", midi_file)
+    return progressions
+
+
+def make_dataset_chords(midi_dir, output_file):
+    data = load_chords(midi_dir)
+    numpy.savez_compressed(output_file, data)
--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+import datetime
 import glob
 import keras
 from keras.layers import *
 from keras.models import *
 import numpy
 import os
+import random
 import tensorflow
 
 config = tensorflow.ConfigProto()
@@ -13,11 +15,12 @@ keras.backend.set_session(session)
 
 notes = 24
 
+
 def low_high(sequence):
     chords = sequence.shape[0]
     low = notes - 1
     high = 0
-    for chord in range(chords):
+    for chord in range(1, chords):
         low_candidate = numpy.argmax(sequence[chord, :])
         high_candidate = notes - numpy.argmax(sequence[chord, ::-1])
         if low_candidate < low:
@@ -26,37 +29,65 @@ def low_high(sequence):
             high = high_candidate
     return low, high
 
-def generate(sequence):
+
+def augment(sequence):
     chords = sequence.shape[0]
     low, high = low_high(sequence)
     transpositions = notes - high + 1
-    data = numpy.empty((transpositions, chords, notes))
+    data = []
     for i in range(transpositions):
+        progression = numpy.empty((chords, notes))
         for chord in range(chords):
-            data[i, chord, :] = numpy.concatenate((
+            progression[chord, :] = numpy.concatenate((
                 numpy.zeros((i,)),
                 sequence[chord, low:high],
                 numpy.zeros((notes - (high - low) - i,))
             ))
-    return data, numpy.roll(data, -1, axis=1)
+        data.append(progression)
+    return data
 
-def generator(data_dir):
+
+def generator(data_dir, seed=None):
     data_files = sorted(glob.glob(os.path.join(data_dir, '*.npz')))
-    for data_file in data_files:
-        data = numpy.load(data_file)
-        progressions = data.shape[0]
-        for progression in progressions:
-            yield generate(data[progression, :, :])
+    if seed is not None:
+        random.seed(seed)
+    while True:
+        for data_file in data_files:
+            sequences = []
+            data = numpy.load(data_file)
+            for key in data:
+                sequences.append(data[key])
+            augmented = []
+            for sequence in sequences:
+                sequence = sequence[0, :, :]
+                sequence = numpy.insert(sequence, 0, numpy.zeros((notes,)), axis=0)
+                augmented.extend(augment(sequence))
+            random.shuffle(augmented)
+            for sequence in augmented:
+                yield sequence[numpy.newaxis, :, :], numpy.vstack([sequence[1:, :], sequence[1, :]])[numpy.newaxis, :, :]
 
-def init_model():
-    input_layer = Input((None, notes))
-    hidden_layer = Bidirectional(LSTM(128, return_sequences=True, recurrent_dropout=0.2), merge_mode='concat')(input_layer)
-    output_layer = TimeDistributed(Dense(notes, activation='sigmoid'))(hidden_layer)
-    model = Model(input_layer, output_layer)
+
+def init_model(submodel):
+    model = None
+    if submodel == 'chords':
+        input_layer = Input((None, notes))
+        hidden_layer = Bidirectional(LSTM(128, return_sequences=True, recurrent_dropout=0.2), merge_mode='concat')(input_layer)
+        output_layer = TimeDistributed(Dense(notes, activation='sigmoid'))(hidden_layer)
+        model = Model(input_layer, output_layer)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
     return model
 
-def train(data_dir, steps_per_epoch, epochs, callbacks):
-    model = init_model()
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+def train(submodel, data_dir, steps_per_epoch, epochs, callbacks=None, seed=None):
+    model = init_model(submodel)
     model.summary()
-    model.fit_generator(generator(data_dir), steps_per_epoch, epochs=epochs, callbacks=callbacks)
+    model.fit_generator(generator(data_dir, seed), steps_per_epoch, epochs=epochs, callbacks=callbacks)
+    return model
+
+
+def evaluate(model, data_dir, steps, seed=None):
+    return model.evaluate_generator(generator(data_dir, seed), steps)
+
+
+def predict(model, x):
+    return model.predict(x)
